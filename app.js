4e0412df4b974b57982e7270c0798b4f61128c7c
Benji Thompson <thompson.benji@gmail.com>
2021-05-11 11:54:40 -0400

    Game logic pseudocode added. Colors obtained for button selection ux, and chromatic notes added in comments.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Connected to the matrix');
+/*
+[] - Play button will initiate game
+[] - Random color will light up and emit tone.
+[] - Player will press button(s) to match the random sequence.
+[] - After each successfull turn by player, game will add additional random button to sequence and replay entire sequence for the player.
+[] - After game plays the updated sequence, player will attempt to match.
+[] - This process will repeat until player makes a mistake and the game ends.
+[] - When the game ends, the previous sequence will clear. Pressing play will start a new round, with a new button sequence.
+[] - Game will keep track of longest sequence and display to player.
+
+
+COLOR CHANGE ON SELECTION~>
+green: 53FFED
+red: FF94BC
+yellow: FFFFB3
+blue: 5ED7FF
+
+NOTES PLAYED ON COLOR SELECTION~>
+green: E (octave lower than blue)
+red: A
+yellow: C#/Db
+blue: E
+
+
+*/
