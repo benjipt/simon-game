ed7b1a90c4d0fda9214560c03c4149a6d06e920f
Benji Thompson <thompson.benji@gmail.com>
2021-09-23 12:36:37 -0400

    Revert "refactored so that only one instance of each sound is created."
    This reverts commit 38309797a3f8b51866f28eb270f9fb31b77f5b3b.

--- app.js
+++ app.js
@@ -50,40 +50,15 @@ const colors = [
     }
 ];
 
-const greenTone = new Audio('sounds/green-button.mp3');
-greenTone.preload = 'auto';
-greenTone.volume = 0.2;
-const redTone = new Audio('sounds/red-button.mp3');
-redTone.preload = 'auto';
-redTone.volume = 0.2;
-const yellowTone = new Audio('sounds/yellow-button.mp3');
-yellowTone.preload = 'auto';
-yellowTone.volume = 0.2;
-const blueTone = new Audio('sounds/blue-button.mp3');
-blueTone.preload = 'auto';
-blueTone.volume = 0.2;
-const gameoverTone = new Audio('sounds/gameover-tone.mp3');
-gameoverTone.preload = 'auto';
-gameoverTone.volume = 0.6;
-
 const playSound = color => {
     if (currentGame) {
-        switch (color) {
-            case 'green':
-                greenTone.play();
-                break;
-            case 'red':
-                redTone.play();
-                break;
-            case 'yellow':
-                yellowTone.play()
-                break;
-            case 'blue':
-                blueTone.play();
-                break;
-        }
+        const tone = new Audio(`sounds/${color}-button.mp3`);
+        tone.volume = 0.2;
+        tone.play();
     } else {
-        gameoverTone.play()
+        const tone = new Audio('sounds/gameover-tone.mp3');
+        tone.volume = 0.6;
+        tone.play();
     }
 };
 
